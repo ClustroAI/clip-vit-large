eee3c9edede474adcb933aae4338c8c89f4c1c14
totoleon <hailong.leon@gmail.com>
2023-09-07 22:29:01 -0700

    Update model_invoke.py

--- model_invoke.py
+++ model_invoke.py
@@ -4,16 +4,21 @@ import json
 
 from transformers import CLIPProcessor, CLIPModel
 
+# Loading the model pipeline
 model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
 
+# Invoke function
 def invoke(input_text):
+    # Parsing input
     input_json = json.loads(input_text)
     image_url = input_json['image_url']
     image = Image.open(requests.get(image_url, stream=True).raw)
     text_arr = input_json['text'].split(",")
     inputs = processor(text=text_arr, images=image, return_tensors="pt", padding=True)
+    # Run inference
     outputs = model(**inputs)
-    logits_per_image = outputs.logits_per_image # this is the image-text similarity score
-    probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
+    logits_per_image = outputs.logits_per_image
+    probs = logits_per_image.softmax(dim=1)
+    # Return the result
     return [text_arr[i]+": "+str(probs.tolist()[0][i]) for i in range(0,len(text_arr))]
